491f090e4f6ec0cd8b2585892f0ca8ab9394ee33
vsklamm <klammvs@gmail.com>
2021-09-25 21:40:47 +0300

    lab3: add AddProductServletTest

---
+++ lab3/src/main/test/ru/vsklamm/sd/refactoring/AddProductServletTest.java
@@ -0,0 +1,106 @@
+package ru.vsklamm.sd.refactoring;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
+import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class AddProductServletTest {
+    private final StringWriter writer = new StringWriter();
+    private static final String DATABASE = "jdbc:sqlite:test.db";
+
+    private static final String CREATE_PRODUCT =
+            "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+    private static final String DROP_PRODUCT =
+            "DROP TABLE IF EXISTS PRODUCT";
+
+    @Mock
+    private HttpServletRequest mockRequest;
+
+    @Mock
+    private HttpServletResponse mockResponse;
+
+    private void addOneProduct(final String name, final String price) throws IOException {
+        when(mockRequest.getParameter("name")).thenReturn(name);
+        when(mockRequest.getParameter("price")).thenReturn(price);
+        new AddProductServlet().doGet(mockRequest, mockResponse);
+    }
+
+    private void runSQL(String sql) {
+        try (Connection c = DriverManager.getConnection(DATABASE)) {
+            var statement = c.createStatement();
+            statement.executeUpdate(sql);
+            statement.close();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Before
+    public void setMocks() throws IOException {
+        MockitoAnnotations.openMocks(this);
+        when(mockResponse.getWriter()).thenReturn(new PrintWriter(writer));
+    }
+
+    @Before
+    public void createProductDB() {
+        runSQL(CREATE_PRODUCT);
+    }
+
+    @After
+    public void dropProductDB() {
+        runSQL(DROP_PRODUCT);
+    }
+
+    @Test
+    @DisplayName("testing adding one product")
+    public void addOneTest() throws IOException {
+        addOneProduct("product", "1");
+        var result = writer.toString();
+        assertTrue(result.contains("OK"));
+        new GetProductsServlet().doGet(mockRequest, mockResponse);
+        result = writer.toString();
+        assertTrue(result.contains("product"));
+    }
+
+    @Test
+    @DisplayName("testing adding many products")
+    public void addManyTest() throws IOException {
+        addOneProduct("product1", "1");
+        var result = writer.toString();
+        assertTrue(result.contains("OK"));
+
+        addOneProduct("product2", "2");
+        result = writer.toString();
+        assertTrue(result.contains("OK"));
+
+        addOneProduct("product3", "3");
+        result = writer.toString();
+        assertTrue(result.contains("OK"));
+
+        new GetProductsServlet().doGet(mockRequest, mockResponse);
+        result = writer.toString();
+        assertTrue(result.contains("product1"));
+        assertTrue(result.contains("product2"));
+        assertTrue(result.contains("product3"));
+    }
+}
